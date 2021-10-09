83add7ceff0860a809b861ff31cc5cf3878aa49e
aymen2782 <aymen.sellaouti@gmail.com>
2021-10-09 11:45:37 +0100

    exemple controller

--- src/Controller/SecondController.php
+++ src/Controller/SecondController.php
@@ -3,14 +3,16 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class SecondController extends AbstractController
 {
     #[Route('/second/{name}', name: 'second')]
-    public function index($name): Response
+    public function index(Request $request, $name): Response
     {
+        dump($request);
         return $this->render('second/index.html.twig', [
             'controller_name' => 'SecondController',
             'name' => $name,
