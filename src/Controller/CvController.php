44934a15b98780a3250aea4115d751495c942021
aymen2782 <aymen.sellaouti@gmail.com>
2021-10-12 09:46:07 +0100

    session

---
+++ src/Controller/CvController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CvController extends AbstractController
+{
+    #[Route("cv/{name}/{firstname}/{age}/{section}", name: 'cv.home')]
+    public function cv(Request $request, $name, $firstname, $age, $section) {
+        $session = $request->getSession();
+        return $this->render('cv/cv.html.twig', [
+            'name' => $name,
+            'firstname' => $firstname,
+            'age' => $age,
+            'section' => $section,
+        ]);
+    }
+}
--- src/Controller/FirstController.php
+++ src/Controller/FirstController.php
@@ -2,10 +2,13 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
-class FirstController
+class FirstController extends AbstractController
 {
     /**
      * @Route("/first", name="home")
@@ -23,6 +26,30 @@ class FirstController
             </html>
         ');
         return $response;
+    }
+
+    #[Route("session", name: "session.nb.visite")]
+    public function session(Request $request) {
+        //Je récupére ma session
+        $session = $request->getSession();
+        //Je vérifie si j'ai la variable nbVisite
+            // Si oui
+            if ($session->has('nbVisite')) {
+                // incrementer
+                $nbVisite = $session->get('nbVisite');
+                $nbVisite++;
+                $session->set('nbVisite', $nbVisite);
+            // si non
+            } else {
+                // on crée la variable et on l initialise à 1
+                $nbVisite = 1;
+                // on la met dans la session
+                $session->set('nbVisite', $nbVisite);
+                // on crée le flashBag
+                $this->addFlash('info', "Bienvenu dans notre plateforme c'est votre première visite");
+            }
+        // on renvoi la twig
+        return $this->render('session/index.html.twig');
     }
 
 }
