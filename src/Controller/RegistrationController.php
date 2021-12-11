b3d381298eb240f1180641e704a5ef05be4de16c
aymen2782 <aymen.sellaouti@gmail.com>
2021-12-11 14:02:32 +0100

    authenticate user after login

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -12,12 +12,13 @@ use Symfony\Component\Mailer\Transport\Smtp\Auth\AuthenticatorInterface;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Guard\GuardAuthenticatorHandler;
+use Symfony\Component\Security\Http\Authentication\UserAuthenticatorInterface;
 
 class RegistrationController extends AbstractController
 {
 
     #[Route('/register', name: 'app_register')]
-    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasherInterface, LoginFormAuthenticator $authenticator, AuthenticatorInterface $guardHandler): Response
+    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasherInterface, LoginFormAuthenticator $authenticator, UserAuthenticatorInterface  $userAuthenticator): Response
     {
         $user = new User();
         $form = $this->createForm(RegistrationFormType::class, $user);
@@ -36,11 +37,11 @@ class RegistrationController extends AbstractController
             $entityManager->persist($user);
             $entityManager->flush();
             // do anything else you need here, like send an email
-
+            $userAuthenticator->authenticateUser($user, $authenticator, $request);
             return $this->redirectToRoute('personne.list.alls');
         }
 
-//        $guardHandler->authenticate($user);
+
         return $this->render('registration/register.html.twig', [
             'registrationForm' => $form->createView(),
         ]);
